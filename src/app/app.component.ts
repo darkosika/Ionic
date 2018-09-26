3f71febb22959704b86d716cb5c912ca28313baa
Unknown <osmnkpln03ozgurluk@gmail.com>
2018-09-26 16:52:14 +0300

    Side Menu

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component,ViewChild} from '@angular/core';
+import { Platform,Nav } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { MenuController } from 'ionic-angular';
+import { HomePage } from '../pages/home/home';
+import { BadgesPage } from '../pages/badges/badges';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = HomePage;
+  pages: Array<{ title: string, component: any }>;
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public menuCtrl: MenuController) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Badges', component: BadgesPage }
+    ];
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
+
---
+++ src/pages/home/home.ts
@@ -0,0 +1,124 @@
+import { Component } from '@angular/core';
+import { NavController, Checkbox } from 'ionic-angular';
+import { Platform, ActionSheetController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  testCheckboxOpen: boolean;
+  testCheckboxResult;
+  constructor(public alertCtrl: AlertController,public platform: Platform,
+    public actionsheetCtrl: ActionSheetController,public navCtrl: NavController) {
+
+  }
+  
+ 
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  doPromptCheckBox(){
+    let alert=this.alertCtrl.create();
+    alert.setTitle("Select Country:");
+    alert.addInput({
+      type:'checkbox',
+      label: 'Turkey',
+      value:'Turkey',
+      checked:true
+    });
+    alert.addInput({
+      type:'checkbox',
+      label:'Greece',
+      value:'Greece'
+    });
+    alert.addInput({
+      type:'checkbox',
+      label:'Germany',
+      value:'Germany'
+    });
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Okay',
+      handler: data => {
+        console.log('Checkbox data:', data);
+        this.testCheckboxOpen = false;
+        this.testCheckboxResult = data;
+      }
+    });
+    alert.present().then(() => {
+      this.testCheckboxOpen = true;
+    });
+  }
+}
